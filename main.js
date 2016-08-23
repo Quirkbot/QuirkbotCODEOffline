12e5037fbc4acd5d55270db3de71529add20a943
Murilo Polese <murilopolese@gmail.com>
2016-08-23 11:32:46 +0200

    Starting compiler apps as a child processes

--- main.js
+++ main.js
@@ -2,7 +2,9 @@ require( 'dotenv' ).config();
 var express = require( 'express' )
 var fork = require('child_process').fork;
 // TODO: Start api
-// TODO: Start compiler process
+// Start compiler process
+var compilerServer = fork( './compiler/server.js' )
+var compilerWorker = fork( './compiler/compiler.js' )
 // Serve CODE;
 var code = express()
 code.use( express.static( './dist_polymer' ) )
