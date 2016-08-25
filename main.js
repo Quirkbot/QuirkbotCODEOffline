e95770c93bfd3be2fea5f22b6bfb2d227b4267cd
Murilo Polese <murilopolese@gmail.com>
2016-08-25 19:38:45 +0200

    Inject a gui.js on every page

--- main.js
+++ main.js
@@ -1,5 +1,4 @@
 var express = require( 'express' )
-var fork = require('child_process').fork;
 var fork = require('child_process').fork
 
 // Serve CODE;
@@ -12,8 +11,8 @@ code.listen( process.env.PORT || 8000 )
 process.env.NODE_ENV = "lite"
 process.env.PORT = 8001
 process.env.MONGO_URL = "tingodb://compiler_db"
-var compilerServer = fork( './compiler/server.js' )
 process.env.WEB_CONCURRENCY = 1
+var compilerServer = fork( './compiler/server.js' )
 var compilerWorker = fork( './compiler/compiler.js' )
 
 // Start api
@@ -22,7 +21,5 @@ process.env.NODE_ENV = "lite"
 var api = require( './api/app' )
 
 var cleanExit = function() { process.exit() }
-process.on('SIGINT', cleanExit) // catch ctrl-c
-process.on('SIGTERM', cleanExit) // catch kill
 process.on( 'SIGINT', cleanExit ) // catch ctrl-c
 process.on( 'SIGTERM', cleanExit ) // catch kill
