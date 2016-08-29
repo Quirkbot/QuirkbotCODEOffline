2715aa81ad8f344fc1c454c7a183aafc0273011d
Murilo Polese <murilopolese@gmail.com>
2016-08-29 16:37:11 +0200

    Fix paths for cross compilation

--- main.js
+++ main.js
@@ -1,24 +1,36 @@
-var express = require( 'express' )
+var express = require('express')
 var fork = require('child_process').fork
+var path = require('path')
+
+var modulePath = function( module ){
+	return path.resolve( require.resolve( path.join( module, 'package.json' ) ), '..' )
+}
 
 // Serve CODE;
-process.env.PORT = 8000
+process.env.PORT = 9510
 var code = express()
-code.use( express.static( __dirname + '/dist_polymer' ) )
+code.use( express.static( path.resolve( __dirname, 'dist_polymer' ) ) )
 code.listen( process.env.PORT || 8000 )
 
 // Start compiler process
 process.env.NODE_ENV = "lite"
-process.env.PORT = 8001
+process.env.PORT = 9511
 process.env.MONGO_URL = "tingodb://compiler_db"
 process.env.WEB_CONCURRENCY = 1
-var compilerServer = fork( './compiler/server.js' )
-var compilerWorker = fork( './compiler/compiler.js' )
+var compilerServer = fork(
+	path.resolve( modulePath( 'quirkbot-compiler' ), 'server.js' )
+)
+var compilerWorker = fork(
+	path.resolve( modulePath( 'quirkbot-compiler' ), 'compiler.js' )
+)
 
 // Start api
-process.env.PORT = 8002
+process.env.PORT = 9512
 process.env.NODE_ENV = "lite"
-var api = require( './api/app' )
+process.env.DB_URL = path.resolve( 'api_db' );
+var api = require(
+	path.resolve( modulePath( 'quirkbot-api' ), 'app.js' )
+)
 
 var cleanExit = function() { process.exit() }
 process.on( 'SIGINT', cleanExit ) // catch ctrl-c
