e5bbb91d3a5098e9c8084d9b54cf276f15040bf9
Murilo Polese <murilopolese@gmail.com>
2016-08-31 14:09:33 +0200

    Wait until process are started to run app

--- main.js
+++ main.js
@@ -1,37 +1,53 @@
-var express = require('express')
-var fork = require('child_process').fork
-var path = require('path')
-
+var express = require( 'express' )
+var fork = require( 'child_process' ).fork
+var path = require( 'path' )
 var modulePath = function( module ){
 	return path.resolve( require.resolve( path.join( module, 'package.json' ) ), '..' )
 }
 
-// Serve CODE;
-process.env.PORT = 9510
-var code = express()
-code.use( express.static( path.resolve( __dirname, 'dist_polymer' ) ) )
-code.listen( process.env.PORT || 8000 )
+// Load configuration file
+var config = require( path.resolve( __dirname, 'config.json' ) );
 
-// Start compiler process
+// Set global environment
 process.env.NODE_ENV = "lite"
-process.env.PORT = 9511
-process.env.MONGO_URL = "tingodb://compiler_db"
-process.env.WEB_CONCURRENCY = 1
-var compilerServer = fork(
-	path.resolve( modulePath( 'quirkbot-compiler' ), 'server.js' )
-)
-var compilerWorker = fork(
-	path.resolve( modulePath( 'quirkbot-compiler' ), 'compiler.js' )
-)
 
-// Start api
-process.env.PORT = 9512
-process.env.NODE_ENV = "lite"
-process.env.DB_URL = path.resolve( 'api_db' );
-var api = require(
-	path.resolve( modulePath( 'quirkbot-api' ), 'app.js' )
-)
+var startApi = function() {
+	// Start api
+	process.env.DISK_DB_PATH = path.resolve( __dirname, 'db', 'api.db' );
+	process.env.PORT = config.ports.api
+	process.env.LITE_NICKNAME = config.credentials.nickname
+	process.env.LITE_PASSWORD = config.credentials.password
+	process.env.LITE_EMAIL = config.credentials.email
+	var api = require(
+		path.resolve( modulePath( 'quirkbot-api' ), 'app.js' )
+	)
+}
 
+var startCompiler = function() {
+	// Start compiler process
+	process.env.DISK_DB_PATH = path.resolve( __dirname, 'db', 'compiler.db' )
+	process.env.PORT = config.ports.compiler
+	process.env.WEB_CONCURRENCY = 1
+	var compilerServer = fork(
+		path.resolve( modulePath( 'quirkbot-compiler' ), 'server.js' )
+	)
+	var compilerWorker = fork(
+		path.resolve( modulePath( 'quirkbot-compiler' ), 'compiler.js' )
+	)
+}
+
+var startCode = function() {
+	// Serve CODE;
+	var code = express()
+	code.use( express.static( path.resolve( __dirname, 'dist_polymer' ) ) )
+	code.listen( config.ports.code )
+}
+
+startApi();
+startCompiler();
+startCode();
+
+// Graceful shutdown, kind of
 var cleanExit = function() { process.exit() }
 process.on( 'SIGINT', cleanExit ) // catch ctrl-c
 process.on( 'SIGTERM', cleanExit ) // catch kill
