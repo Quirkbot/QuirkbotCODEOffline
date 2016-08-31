c75513773930ec66110415656cac6068cc6b42ce
Murilo Polese <murilopolese@gmail.com>
2016-08-31 14:07:40 +0200

    Gulp process to patch and inject script

--- gulpfile.js
+++ gulpfile.js
@@ -6,7 +6,7 @@ var gulp = require('gulp');
 var file = require('gulp-file');
 var jeditor = require('gulp-json-editor');
 var del = require('del');
-var config = require('./gulpconfig.json');
+var config = require('./config.json');
 
 gulp.task('patch-extension', function () {
 	return gulp.src("node_modules/quirkbot-chrome-app/manifest.json")
@@ -24,14 +24,30 @@ gulp.task('patch-extension', function () {
 				"node_modules/quirkbot-chrome-app"),
 				{ overwrite: true }
 			)
-})
+});
+
 gulp.task('patch-code', function () {
 	var template = function( opts ) {
-		return "var USERNAME = '" + opts.nickname + "';var PASSWORD = '" + opts.password + "'";
+		return 'window.QUIRKBOT_CODE_DEFAULT_USER_NICKNAME = "' + opts.nickname + '"; \
+						window.QUIRKBOT_CODE_DEFAULT_USER_PASSWORD = "' + opts.password + '";'
 	}
-	return file('injected_scripts.js', template(config.credentials))
+	return file('injected_script.js', template(config.credentials))
 		.pipe(gulp.dest('dist_polymer', {overwrite: true}));
-})
+});
+
 gulp.task('build', ['patch-extension', 'patch-code']);
-gulp.task('compile', ['build'], function () {});
-gulp.task('deploy', function () {});
+
+gulp.task('package', ['build'], function () {
+	// Run nwjs packaging
+});
+
+gulp.task('deploy', function () {
+	// Deploy to s3
+});
+
+gulp.task('clean', function () {
+	return del( [
+		'node_modules',
+		'dist_polymer'
+	])
+})
